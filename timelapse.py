6367ee01945d7f4865642e6f3e34b17813e4e873
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-03 08:33:51 +0200

    Create timelapse.py

---
+++ timelapse.py
@@ -0,0 +1,13 @@
+ls
+from time import sleep
+import picamera
+
+WAIT_TIME = 30
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (820, 616)
+    camera.rotation = 180
+    print ('go')
+    for filename in camera.capture_continuous('/home/pi/timelapse/output/img{timestamp:%H-%M-%S-%f}.jpg'):
+        sleep(WAIT_TIME)
+        print ('pic')
