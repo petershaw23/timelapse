cd3afd8985280d2275d4a9d5807c0b7ff9a94e73
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-03 16:05:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@
 # -*- coding:utf-8 -*-
 import time
 import picamera
-camera.start_preview(
-    alpha=128, fullscreen=False, window=(0, 0, 640, 480))
+camera = picamera.PiCamera()
+camera.start_preview(alpha=128, fullscreen=False, window=(0, 0, 640, 480))
