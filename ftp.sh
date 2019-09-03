38da37a8ab1b450f8f2dabdf51079862455d4402
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-03 09:18:31 +0200

    Update ftp.sh

--- ftp.sh
+++ ftp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pwd
 #scp timelapse.py hosting126791@188.68.47.235:httpdocs/peter-shaw/timelapse
-rsync -avzh output/ hosting126791@188.68.47.235:httpdocs/peter-shaw/timelapse
+#rsync -avzh output/ hosting126791@188.68.47.235:httpdocs/peter-shaw/timelapse
