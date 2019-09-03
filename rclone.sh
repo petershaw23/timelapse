e9d2c38488a33701b47aed8dcd6280af8ab8f57c
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-03 16:11:39 +0200

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # scp timelapse.py hosting126791@188.68.47.235:httpdocs/peter-shaw/timelapse
-# this uses rclone, see: https://rclone.org/ needs to setup via "rclone config" first
+# this uses rclone, see: https://rclone.org/ needs to setup via "rclone config" first.
 source /home/pi/.profile
 rclone sync -P output/ ftp:httpdocs/peter-shaw/timelapse
 
