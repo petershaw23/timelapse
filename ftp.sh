13238c43dedaa443e4afe58b3be9a6f598a9a846
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-03 08:55:41 +0200

    Update ftp.sh

--- ftp.sh
+++ ftp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /home/pi/.profile
-scp home/pi/timelapse/timelapse.py hosting126791@188.68.47.235:httpdocs/peter-shaw/
+pwd
+#scp home/pi/timelapse/timelapse.py hosting126791@188.68.47.235:httpdocs/peter-shaw/
