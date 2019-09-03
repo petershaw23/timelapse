6a4d0d220939a6697da10a51c27a7929df779cc3
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-03 16:04:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,5 @@
 # -*- coding:utf-8 -*-
 import time
 import picamera
-
-with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
-    camera.start_preview(fullscreen=False, window=(100,100,256,192))
-    time.sleep(2)
-    camera.preview.window=(200,200,256,192)
-    time.sleep(2)
-    camera.preview.window=(0,0,512,384)
-    time.sleep(2)
+camera.start_preview(
+    alpha=128, fullscreen=False, window=(0, 0, 640, 480))
