04207ba637ce77b862fb1b2633acfe29e7eb17ae
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-03 16:25:23 +0200

    Update dark-lapse.py

--- dark-lapse.py
+++ dark-lapse.py
@@ -1,31 +1,16 @@
 
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-#timelapse script by psw
-from time import sleep
-import picamera
-
-WAIT_TIME = 30 # time in seconds
-
-with picamera.PiCamera() as camera:
-    camera.resolution = (820, 616)
-    camera.meter_mode = 'matrix'
-    camera.rotation = 180
-    sleep(2)
-    for filename in camera.capture_continuous('/home/pi/timelapse/output/img{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
-        print('Captured %s' % filename)
-        sleep(WAIT_TIME)
-
-
-
-
+#timelapse script by psw DARK MODE
 
 import picamera
 from time import sleep
 from fractions import Fraction
 
+WAIT_TIME = 30 # time in seconds
+
 with picamera.PiCamera() as camera:
-    camera.resolution = (1280, 720)
+    camera.resolution = (820, 616)
     # Set a framerate of 1/6fps, then set shutter
     # speed to 6s and ISO to 800
     camera.framerate = Fraction(1, 6)
@@ -38,4 +23,6 @@ with picamera.PiCamera() as camera:
     # Finally, capture an image with a 6s exposure. Due
     # to mode switching on the still port, this will take
     # longer than 6 seconds
-    camera.capture('dark.jpg')
+    for filename in camera.capture_continuous('/home/pi/timelapse/output/dark_{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+        print('Captured %s' % filename)
+        sleep(WAIT_TIME)
