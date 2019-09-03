357c9da3251e19f70501b124bbb6e97180888e20
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-03 14:02:14 +0200

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -2,13 +2,18 @@
 # -*- coding:utf-8 -*-
 from time import sleep
 import picamera
+import datetime
+
+Datum = datetime.datetime.now().strftime('%-d.%-m.')
+Uhrzeit = datetime.datetime.now().strftime('%H:%M')
 
 WAIT_TIME = 30
 
 with picamera.PiCamera() as camera:
     camera.resolution = (820, 616)
     camera.rotation = 180
-    print ('go')
+print (Datum, Uhrzeit)
     for filename in camera.capture_continuous('/home/pi/timelapse/output/img{timestamp:%H-%M-%S-%f}.jpg'):
+        print (Datum, Uhrzeit)
         sleep(WAIT_TIME)
-        print ('pic')
+        
