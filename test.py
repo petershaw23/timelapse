540383e205b550f292aa2933e5debd708a9a5560
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-04 09:07:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -14,13 +14,14 @@ with picamera.PiCamera() as camera:
     # camera.resolution = (820, 616) #crop?
     camera.resolution = (1640, 1232) #2x2 binned
     camera.meter_mode = 'matrix' # 'average' 'spot' 'backlit' 'matrix'
-
     camera.rotation = 180
-    camera.drc_strength = 'high' # 'off' 'low' 'medium' 'high'
+    # camera.drc_strength = 'high' # 'off' 'low' 'medium' 'high'
+    
     # for dark mode: Set a framerate of 1/6fps, then set shutter
     # speed to 6s and ISO to 800, exposure mode to off
+    
     camera.framerate = Fraction(1, 6)
-    # camera.shutter_speed = 6000000
+    # camera.shutter_speed = 6000000 #6 seconds
     camera.exposure_mode = 'night' # 'off' 'auto' 'night' 'nightpreview' 'backlight' 'spotlight' 'sports' 'snow' 'beach' 'verylong' 'fixedfps' 'antishake' 'fireworks'
     # camera.iso = 800 # overwrites exposure mode
     camera.awb_mode = 'shade' # 'off' 'auto'  'sunlight'  'cloudy'  'shade'  'tungsten' 'fluorescent'  'incandescent' 'flash' 'horizon'
