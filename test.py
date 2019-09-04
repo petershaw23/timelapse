3fd77509f72d51938afad8c9529670edbb66c68e
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-04 08:36:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-#timelapse script by psw DARK MODE
-
-
-
-  
-
-
+#timelapse script by psw TEST MODE
 
 import picamera
 from time import sleep
@@ -22,10 +16,11 @@ with picamera.PiCamera() as camera:
     # Set a framerate of 1/6fps, then set shutter
     # speed to 6s and ISO to 800
     camera.framerate = Fraction(1, 6)
+    # camera.shutter_speed = 6000000
     camera.exposure_mode = 'night' # 'off' 'auto' 'night' 'nightpreview' 'backlight' 'spotlight' 'sports' 'snow' 'beach' 'verylong' 'fixedfps' 'antishake' 'fireworks'
     # camera.iso = 800
     camera.awb_mode = 'shade' # 'off' 'auto'  'sunlight'  'cloudy'  'shade'  'tungsten' 'fluorescent'  'incandescent' 'flash' 'horizon'
-
-    for filename in camera.capture_continuous('/home/pi/timelapse/output/night_{counter:05d}__{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+    
+    for filename in camera.capture_continuous('/home/pi/timelapse/output/test_{counter:05d}__{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
         print('Captured %s' % filename)
         sleep(WAIT_TIME)
