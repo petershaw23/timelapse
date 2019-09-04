8288b47ec4767213096a0b6486ba7090e7815e60
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-04 08:51:32 +0200

    Update test.py

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ with picamera.PiCamera() as camera:
     camera.framerate = Fraction(1, 6)
     # camera.shutter_speed = 6000000
     camera.exposure_mode = 'night' # 'off' 'auto' 'night' 'nightpreview' 'backlight' 'spotlight' 'sports' 'snow' 'beach' 'verylong' 'fixedfps' 'antishake' 'fireworks'
-    # camera.iso = 800
+    # camera.iso = 800 # overwrites exposure mode
     camera.awb_mode = 'shade' # 'off' 'auto'  'sunlight'  'cloudy'  'shade'  'tungsten' 'fluorescent'  'incandescent' 'flash' 'horizon'
     
     for filename in camera.capture_continuous('/home/pi/timelapse/output/test_{counter:05d}__{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
