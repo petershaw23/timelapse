dbdb3cfd2706d3b920c72a6b13ac057a9821d70f
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-04 09:06:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -17,14 +17,15 @@ with picamera.PiCamera() as camera:
 
     camera.rotation = 180
     camera.drc_strength = 'high' # 'off' 'low' 'medium' 'high'
-    # Set a framerate of 1/6fps, then set shutter
-    # speed to 6s and ISO to 800
+    # for dark mode: Set a framerate of 1/6fps, then set shutter
+    # speed to 6s and ISO to 800, exposure mode to off
     camera.framerate = Fraction(1, 6)
     # camera.shutter_speed = 6000000
     camera.exposure_mode = 'night' # 'off' 'auto' 'night' 'nightpreview' 'backlight' 'spotlight' 'sports' 'snow' 'beach' 'verylong' 'fixedfps' 'antishake' 'fireworks'
     # camera.iso = 800 # overwrites exposure mode
-    # camera.awb_mode = 'shade' # 'off' 'auto'  'sunlight'  'cloudy'  'shade'  'tungsten' 'fluorescent'  'incandescent' 'flash' 'horizon'
+    camera.awb_mode = 'shade' # 'off' 'auto'  'sunlight'  'cloudy'  'shade'  'tungsten' 'fluorescent'  'incandescent' 'flash' 'horizon'
+    
     for filename in camera.capture_continuous('/home/pi/timelapse/output/test_{counter:05d}__{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
-        print(camera.shutter_speed)
+        print(camera.shutter_speed, camera.iso)
         print('Captured %s' % filename)
         sleep(WAIT_TIME)
