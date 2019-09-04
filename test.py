e57b5241e9679b09bd506f840bba8a08b6c7a610
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-04 08:23:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,45 @@
+
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import time
+#timelapse script by psw DARK MODE
+
+#exposure modes:
+
+  #  'off'
+  #  'auto'
+  #  'night'
+  #  'nightpreview'
+  #  'backlight'
+  #  'spotlight'
+  #  'sports'
+  #  'snow'
+  #  'beach'
+  #  'verylong'
+  #  'fixedfps'
+  #  'antishake'
+  #  'fireworks'
+
+
+
 import picamera
-camera = picamera.PiCamera()
-camera.start_preview(alpha=128, fullscreen=False, window=(0, 0, 640, 480))
+from time import sleep
+from fractions import Fraction
+
+WAIT_TIME = 30 # time in seconds
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (820, 616)
+    # Set a framerate of 1/6fps, then set shutter
+    # speed to 6s and ISO to 800
+    camera.framerate = Fraction(1, 6)
+    camera.exposure_mode = 'night'
+    # camera.iso = 800
+    # Give the camera a good long time to measure AWB
+    # (you may wish to use fixed AWB instead)
+    sleep(10)
+    # Finally, capture an image with a 6s exposure. Due
+    # to mode switching on the still port, this will take
+    # longer than 6 seconds
+    for filename in camera.capture_continuous('/home/pi/timelapse/output/night_{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+        print('Captured %s' % filename)
+        sleep(WAIT_TIME)
