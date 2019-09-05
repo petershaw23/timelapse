2256d4f1db7e304f21fa0201cf46929ffaa49ec8
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-09-05 08:26:41 +0200

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -5,12 +5,23 @@ from time import sleep
 import picamera
 
 WAIT_TIME = 30 # time in seconds
-
 with picamera.PiCamera() as camera:
-    camera.resolution = (820, 616)
-    camera.meter_mode = 'matrix'
+    # camera.resolution = (820, 616) #crop?
+    camera.resolution = (1640, 1232) #2x2 binned
+    camera.meter_mode = 'matrix' # 'average' 'spot' 'backlit' 'matrix'
     camera.rotation = 180
-    sleep(2)
-    for filename in camera.capture_continuous('/home/pi/timelapse/output/lapse_{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+    # camera.drc_strength = 'high' # 'off' 'low' 'medium' 'high'
+    
+    # for dark mode: Set a framerate of 1/6fps, then set shutter
+    # speed to 6s and ISO to 800, exposure mode to off
+    
+    #camera.framerate = Fraction(1, 6)
+    # camera.shutter_speed = 6000000 #6 seconds
+    camera.exposure_mode = 'backlight' # 'off' 'auto' 'night' 'nightpreview' 'backlight' 'spotlight' 'sports' 'snow' 'beach' 'verylong' 'fixedfps' 'antishake' 'fireworks'
+    # camera.iso = 800 # overwrites exposure mode
+    camera.awb_mode = 'sunlight' # 'off' 'auto'  'sunlight'  'cloudy'  'shade'  'tungsten' 'fluorescent'  'incandescent' 'flash' 'horizon'
+    
+    for filename in camera.capture_continuous('/home/pi/timelapse/output/lapse_{counter:05d}__{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+        print(camera.shutter_speed, camera.iso)
         print('Captured %s' % filename)
         sleep(WAIT_TIME)
